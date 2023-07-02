8a84a721b94ff73d42099a91a71c0ffcc8c2fd03
nodarnozadze <125303289+nodarnozadze@users.noreply.github.com>
2023-07-02 21:53:17 +0400

    commitv2

--- midterm.sh
+++ midterm.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
+
 set -o errexit
 set -o nounset
 set -o pipefail
 
-# Enable tracing if the BASH_TRACE environment variable is set to 1
+# Enable tracing if BASH_TRACE is set to 1
 if [[ "${BASH_TRACE:-0}" == "1" ]]; then
     set -o xtrace
 fi
 
-# Check if the GITHUB_PERSONAL_ACCESS_TOKEN environment variable is set
+# Check if GITHUB_PERSONAL_ACCESS_TOKEN environment variable is set
 if [ -z "$GITHUB_PERSONAL_ACCESS_TOKEN" ]; then
     echo "GITHUB_PERSONAL_ACCESS_TOKEN environment variable is missing"
     exit 1
 fi
 
-# Change the working directory to the script's directory
+# Change to the directory of the script
 cd "$(dirname "$0")"
 
-# Check if four arguments are provided, otherwise exit with an error message
-if [ "$#" -eq 4 ]; then
-    echo > /dev/null
-else
+# Check if the script was provided with four arguments
+if [ "$#" -ne 4 ]; then
     echo "The script was not provided with four arguments."
     echo "Usage: ./mid-term.sh CODE_REPO_URL CODE_BRANCH_NAME REPORT_REPO_URL REPORT_BRANCH_NAME"
     exit 1
 fi
 
-# Assign the arguments to variables
+# Assign argument values to variables
 CODE_REPO_URL="$1"
 CODE_BRANCH_NAME="$2"
 REPORT_REPO_URL="$3"
 REPORT_BRANCH_NAME="$4"
 
-# Extract the repository names and owner from the repository URLs
-CODE_REPOSITORY_NAME=$(basename "$CODE_REPO_URL" .git)
+# Extract repository name and owner from the code repository URL
+REPOSITORY_NAME_CODE=$(basename "$CODE_REPO_URL" .git)
 REPOSITORY_OWNER=$(basename "$(dirname "$CODE_REPO_URL")")
-REPORT_REPOSITORY_NAME=$(basename "$REPORT_REPO_URL" .git)
+
+# Extract repository name from the report repository URL
+REPOSITORY_NAME_REPORT=$(basename "$REPORT_REPO_URL" .git)
 
 # Create temporary directories for code and report repositories
-CODE_REPOSITORY_PATH=$(mktemp --directory)
-REPORT_REPOSITORY_PATH=$(mktemp --directory)
+REPOSITORY_PATH_CODE=$(mktemp --directory)
+REPOSITORY_PATH_REPORT=$(mktemp --directory)
 
-# Set initial values for test results
 PYTEST_RESULT=0
 BLACK_RESULT=0
 
@@ -72,7 +72,7 @@ else
     exit 1
 fi
 
-# Check if pytest and black commands are available
+# Check if pytest and black are installed
 if ! pytest --version >/dev/null 2>&1; then
     echo "pytest is not installed"
     exit 1
@@ -83,16 +83,213 @@ if ! black --version >/dev/null 2>&1; then
     exit 1
 fi
 
-# Function to perform cleanup actions
+# Function to clean up temporary files and directories
 cleanup() {
     echo "Cleaning up..."
-    if [ -d "$CODE_REPOSITORY_PATH" ]; then
-        rm -rf "$CODE_REPOSITORY_PATH"
-        echo "Deleted CODE_REPOSITORY_PATH"
+    if [ -d "$REPOSITORY_PATH_CODE" ]; then
+        rm -rf "$REPOSITORY_PATH_CODE"
+        echo "Deleted REPOSITORY_PATH_CODE"
     fi
-    if [ -d "$REPORT_REPOSITORY_PATH" ]; then
-        rm -rf "$REPORT_REPOSITORY_PATH"
-        echo "Deleted REPORT_REPOSITORY_PATH"
+    if [ -d "$REPOSITORY_PATH_REPORT" ]; then
+        rm -rf "$REPOSITORY_PATH_REPORT"
+        echo "Deleted REPOSITORY_PATH_REPORT"
     fi
     if [ -f "$PYTEST_REPORT_PATH" ]; then
-        rm -rf "$PY.
+        rm -rf "$PYTEST_REPORT_PATH"
+        echo "Deleted PYTEST_REPORT_PATH"
+    fi
+    if [ -f "$BLACK_REPORT_PATH" ]; then
+        rm -rf "$BLACK_REPORT_PATH"
+        echo "Deleted BLACK_REPORT_PATH"
+    fi
+    if [ -f "$BLACK_OUTPUT_PATH" ]; then
+        rm -rf "$BLACK_OUTPUT_PATH"
+        echo "Deleted BLACK_OUTPUT_PATH"
+    fi   
+}
+
+# Trap signals to perform cleanup on script termination
+trap cleanup INT EXIT ERR SIGINT SIGTERM
+
+# Function to make a GET request to GitHub API
+github_api_get_request() {
+    curl --request GET \
+        --header "Accept: application/vnd.github+json" \
+        --header "Authorization: Bearer $GITHUB_PERSONAL_ACCESS_TOKEN" \
+        --header "X-GitHub-Api-Version: 2022-11-28" \
+        --output "$2" \
+        --silent \
+        "$1"
+}
+
+# Function to make a POST request to GitHub API
+github_post_request() {
+    curl --request POST \
+        --header "Accept: application/vnd.github+json" \
+        --header "Authorization: Bearer $GITHUB_PERSONAL_ACCESS_TOKEN" \
+        --header "X-GitHub-Api-Version: 2022-11-28" \
+        --header "Content-Type: application/json" \
+        --silent \
+        --output "$3" \
+        --data-binary "@$2" \
+        "$1"
+}
+
+# Function to update a JSON file using jq
+jq_update() {
+    local IO_PATH=$1
+    local TEMP_PATH=$(mktemp)
+    shift
+    cat "$IO_PATH" | jq "$@" > "$TEMP_PATH"
+    mv "$TEMP_PATH" "$IO_PATH"
+}
+
+# Clone the code repository to the code directory
+git clone "$CODE_REPO_URL" "$REPOSITORY_PATH_CODE"
+cd "$REPOSITORY_PATH_CODE"
+git switch "$CODE_BRANCH_NAME"
+
+# Get the last commit hash
+LAST_COMMIT="$(git log -n 1 --format=%H)"
+
+while true; do
+    # Fetch changes from the code repository
+    git fetch "$1" "$2" > /dev/null 2>&1
+    CHECK_COMMIT=$(git rev-parse FETCH_HEAD)
+
+    # Check if there are new commits
+    if [ "$CHECK_COMMIT" != "$LAST_COMMIT" ]; then
+        # Get the list of commits between the last commit and the new commit
+        COMMITS=$(git log --pretty=format:"%H" --reverse "$LAST_COMMIT..$CHECK_COMMIT")
+        echo "$COMMITS"
+        LAST_COMMIT=$CHECK_COMMIT
+
+        for COMMIT in $COMMITS; do
+            PYTEST_REPORT_PATH=$(mktemp)
+            BLACK_OUTPUT_PATH=$(mktemp)
+            BLACK_REPORT_PATH=$(mktemp)
+
+            # Checkout the commit
+            git checkout "$COMMIT"
+
+            # Get the author's email
+            AUTHOR_EMAIL=$(git log -n 1 --format="%ae" HEAD)
+
+            # Run pytest and save the report
+            if pytest --verbose --html="$PYTEST_REPORT_PATH" --self-contained-html; then
+                PYTEST_RESULT=$?
+                echo "PYTEST SUCCEEDED $PYTEST_RESULT"
+            else
+                PYTEST_RESULT=$?
+                echo "PYTEST FAILED $PYTEST_RESULT"
+            fi
+
+            echo "\$PYTEST_RESULT = $PYTEST_RESULT \$BLACK_RESULT=$BLACK_RESULT"
+
+            # Run black and check code formatting
+            if black --check --diff *.py > "$BLACK_OUTPUT_PATH"; then
+                BLACK_RESULT=$?
+                echo "BLACK SUCCEEDED $BLACK_RESULT"
+            else
+                BLACK_RESULT=$?
+                echo "BLACK FAILED $BLACK_RESULT"
+                cat "$BLACK_OUTPUT_PATH" | pygmentize -l diff -f html -O full,style=solarized-light -o "$BLACK_REPORT_PATH"
+            fi
+
+            echo "\$PYTEST_RESULT = $PYTEST_RESULT \$BLACK_RESULT=$BLACK_RESULT"
+
+            # Check if the report repository has already been cloned
+            if [ -d "$REPOSITORY_PATH_REPORT" ] && [ "$(ls -A "$REPOSITORY_PATH_REPORT")" ]; then
+                echo "Directory $REPOSITORY_PATH_REPORT exists and is not empty. Skipping cloning."
+            else
+                git clone "$REPORT_REPO_URL" "$REPOSITORY_PATH_REPORT"
+            fi
+
+            pushd "$REPOSITORY_PATH_REPORT"
+            git switch "$REPORT_BRANCH_NAME"
+
+            # Create a directory for the report
+            REPORT_PATH="${COMMIT}-$(date +%s)"
+            mkdir --parents "$REPORT_PATH"
+            cp "$PYTEST_REPORT_PATH" "$REPORT_PATH/pytest.html"
+
+            # Copy the black report if it exists
+            if [ -s "$BLACK_REPORT_PATH" ]; then
+                cp "$BLACK_REPORT_PATH" "$REPORT_PATH/black.html"
+            fi
+
+            git add "$REPORT_PATH"
+            git commit -m "$COMMIT report."
+            git push
+            popd
+
+            # Check the test and formatting results
+            if (( ($PYTEST_RESULT != 0) || ($BLACK_RESULT != 0) )); then
+                AUTHOR_USERNAME=""
+                RESPONSE_PATH=$(mktemp)
+                github_api_get_request "https://api.github.com/search/users?q=$AUTHOR_EMAIL" "$RESPONSE_PATH"
+
+                TOTAL_USER_COUNT=$(cat "$RESPONSE_PATH" | jq ".total_count")
+
+                if [[ $TOTAL_USER_COUNT == 1 ]]; then
+                    USER_JSON=$(cat "$RESPONSE_PATH" | jq ".items[0]")
+                    AUTHOR_USERNAME=$(cat "$RESPONSE_PATH" | jq --raw-output ".items[0].login")
+                fi
+
+                REQUEST_PATH=$(mktemp)
+                RESPONSE_PATH=$(mktemp)
+                echo "{}" > "$REQUEST_PATH"
+
+                BODY+="Automatically generated message\n\n"
+
+                if (( $PYTEST_RESULT != 0 )); then
+                    if (( $BLACK_RESULT != 0 )); then
+                        if [[ "$PYTEST_RESULT" -eq "5" ]]; then
+                            TITLE="${COMMIT::7} Unit tests do not exist in the repository or do not work correctly and formatting test failed."
+                            BODY+="${COMMIT} Unit tests do not exist in the repository or do not work correctly and formatting test failed.\n"
+                        else
+                            TITLE="${COMMIT::7} failed unit and formatting tests."
+                            BODY+="${COMMIT} failed unit and formatting tests.\n"
+                            jq_update "$REQUEST_PATH" '.labels = ["ci-pytest", "ci-black"]'
+                        fi
+                    else
+                        if [[ "$PYTEST_RESULT" -eq "5" ]]; then
+                            TITLE="${COMMIT::7} Unit tests do not exist in the repository or do not work correctly and formatting test passed."
+                            BODY+="${COMMIT} Unit tests do not exist in the repository or do not work correctly and formatting test passed.\n"
+                        else
+                            TITLE="${COMMIT::7} failed unit tests."
+                            BODY+="${COMMIT} failed unit tests.\n"
+                            jq_update "$REQUEST_PATH" '.labels = ["ci-pytest"]'
+                        fi
+                    fi
+                else
+                    TITLE="${COMMIT::7} failed formatting test."
+                    BODY+="${COMMIT} failed formatting test.\n"
+                    jq_update "$REQUEST_PATH" '.labels = ["ci-black"]'
+                fi
+
+                BODY+="Pytest report: https://${REPOSITORY_OWNER}.github.io/${REPOSITORY_NAME_REPORT}/$REPORT_PATH/pytest.html\n"
+                BODY+="Black report: https://${REPOSITORY_OWNER}.github.io/${REPOSITORY_NAME_REPORT}/$REPORT_PATH/black.html\n"
+
+                jq_update "$REQUEST_PATH" '.title = "'"$TITLE"'"'
+                jq_update "$REQUEST_PATH" '.body = "'"$BODY"'"'
+
+                # Create an issue on the code repository
+                github_post_request "https://api.github.com/repos/${REPOSITORY_OWNER}/${REPOSITORY_NAME_CODE}/issues" "$REQUEST_PATH" "$RESPONSE_PATH"
+
+                ISSUE_NUMBER=$(cat "$RESPONSE_PATH" | jq --raw-output ".number")
+
+                # Create a comment on the report repository
+                COMMENT_BODY="Failed tests: https://${REPOSITORY_OWNER}.github.io/${REPOSITORY_NAME_REPORT}/$REPORT_PATH/pytest.html"
+
+                if [[ -n "$AUTHOR_USERNAME" ]]; then
+                    COMMENT_BODY+="\n\n@${AUTHOR_USERNAME}"
+                fi
+
+                github_post_request "https://api.github.com/repos/${REPOSITORY_OWNER}/${REPOSITORY_NAME_REPORT}/issues/${ISSUE_NUMBER}/comments" <(echo "{\"body\":\"${COMMENT_BODY}\"}") "$RESPONSE_PATH"
+            fi
+        done
+    fi
+
+    sleep 30
+done
--- readme.md
+++ readme.md
@@ -1,76 +1,31 @@
-markdown
-Copy code
-# Automated Code Testing and Reporting Script
+Midterm.sh
+Requirements
+Bash Shell
+Git
+Python and Pytest
+GitHub API Token
+JQ
+Instructions
+Install Bash Shell, Git, Python, and Pytest on your system.
 
-This script is designed to automate the process of code testing and reporting for a Git repository. It monitors a code repository for new commits, runs pytest for unit testing and black for code formatting check, generates reports, and creates issues on GitHub if the tests or formatting fail.
+Generate a GitHub API Token with the necessary permissions.
 
-## Prerequisites
+Install JQ using the appropriate package manager for your operating system.
 
-- Git: Make sure Git is installed on your system and accessible from the command line.
-- Python: Install Python and pytest for running unit tests. Use pip to install pytest: `pip install pytest`.
-- Black: Install Black for code formatting check. Use pip to install black: `pip install black`.
-- curl: Ensure that curl is installed to make API requests to GitHub.
+Open the midterm.sh script in a text editor.
 
-## Setup
+Set the GITHUB_PERSONAL_ACCESS_TOKEN environment variable in the script by replacing YOUR_TOKEN_HERE with your GitHub API Token.
 
-1. Clone this repository: `git clone <repository_url>`
-2. Change the working directory to the script's directory: `cd automated-code-testing-and-reporting`
-3. Set the environment variable `GITHUB_PERSONAL_ACCESS_TOKEN` to your GitHub personal access token. The token should have access to the repositories where you want to create issues.
-   ```bash
-   export GITHUB_PERSONAL_ACCESS_TOKEN=<your_access_token>
-Make the script executable: chmod +x automated-code-testing.sh
-Usage
-The script accepts four arguments:
+Save the modified midterm.sh script.
 
-bash
-Copy code
-./automated-code-testing.sh <code_repo_url> <code_branch_name> <report_repo_url> <report_branch_name>
-code_repo_url: The URL of the code repository to monitor for changes.
-code_branch_name: The name of the branch to monitor in the code repository.
-report_repo_url: The URL of the report repository where the test reports and issues will be created.
-report_branch_name: The name of the branch in the report repository.
-Example usage:
+Make the midterm.sh script executable by running chmod +x midterm.sh.
 
-bash
-Copy code
-./automated-code-testing.sh https://github.com/your_username/code_repo.git main https://github.com/your_username/report_repo.git main
-Functionality
-Environment Variable Check: The script checks if the GITHUB_PERSONAL_ACCESS_TOKEN environment variable is set. Make sure to set it before running the script.
+Run the midterm.sh script using ./midterm.sh in your terminal.
 
-Argument Check: The script validates if all four required arguments are provided. If any argument is missing, it will display an error message and exit.
+Monitor the script's output for any issues or failures.
 
-Clone Repositories: The script clones the code and report repositories to temporary directories.
+Review the generated reports by accessing the provided links.
 
-Check Repository and Branch Existence: The script checks if the code and report repositories exist and if the specified branches exist in both repositories. If any of the repositories or branches do not exist, it displays an error message and exits.
+Take appropriate actions based on the test and formatting results.
 
-Check Required Tools: The script checks if pytest and black commands are available. If any of the tools are missing, it displays an error message and exits.
-
-Cleanup Actions: The script sets up cleanup actions to remove temporary directories and files created during execution.
-
-GitHub API Requests: The script includes functions to make GET and POST requests to the GitHub API. These functions are used to fetch user information and create issues on GitHub.
-
-Monitor Code Repository: The script enters an infinite loop to monitor the code repository for changes. It fetches the latest changes, checks for new commits, and processes each commit individually.
-
-Process Commits: For each new commit, the script performs the following actions:
-
-Switches to the commit in the code repository.
-Runs pytest for unit testing and saves the report to a temporary file.
-Runs black for code formatting check and saves the output and diff report to temporary files.
-Checks the test and formatting results and updates the corresponding issue on GitHub, if applicable.
-Create Issues: If the tests or code formatting fail, the script creates or updates an issue on GitHub with the failure details.
-
-Cleanup: At the end of execution, the script removes temporary directories and files.
-
-Contributing
-Contributions are welcome! If you have any suggestions or improvements, feel free to submit a pull request.
-
-License
-This project is licensed under the MIT License.
-
-css
-Copy code
-
-Feel free to modify and adapt the README.md according to your needs and preferences.
-
-
-
+Ensure that you have a good understanding of the requirements and carefully follow the instructions to execute the midterm.sh script successfully.
